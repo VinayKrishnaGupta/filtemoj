9fc35e334105b598346018b3d4ae8277950852cc
Vinay Krishna Gupta <vinaygupta.iitkgp@gmail.com>
2018-05-27 21:12:29 +0530

    unsharp

--- Emojenics/ViewController.m
+++ Emojenics/ViewController.m
@@ -30,12 +30,12 @@
 //
     //GPUImageSmoothToonFilter
     GPUImageToonFilter *filter1 = [[GPUImageToonFilter alloc] init];
-    filter1.threshold = 0.9;
-    filter1.quantizationLevels = 7;
+    filter1.threshold = 0.8;
+    filter1.quantizationLevels = 20;
   //  filter1.blurRadiusInPixels = 2.5;
     
     GPUImageSaturationFilter *filter2 = [[GPUImageSaturationFilter alloc] init];
-    filter2.saturation = 0.1;
+    filter2.saturation = 0.5;
     
     [filter1 addTarget:filter2];
     
@@ -44,9 +44,9 @@
 //    [filter2 addTarget:filter3];
     
     GPUImagePinchDistortionFilter *filter3 = [[GPUImagePinchDistortionFilter alloc]init];
-    filter3.radius = 4;
-    filter3.scale = -0.3;
-    filter3.center = CGPointMake(0.45, 0.43);
+    filter3.radius = 6;
+    filter3.scale = -0.4    ;
+    filter3.center = CGPointMake(0.48, 0.15);
       [filter2 addTarget:filter3];
 //    //GPUImageZoomBlurFilter
 //    GPUImageZoomBlurFilter *filter4 = [[GPUImageZoomBlurFilter alloc] init];
@@ -54,10 +54,26 @@
 //    [filter3 addTarget:filter4];
 //
     //
-    GPUImageWhiteBalanceFilter *filter4 = [[GPUImageWhiteBalanceFilter alloc] init];
-    filter4.temperature = 4500;
-    filter4.tint = 100;
-    [filter3 addTarget:filter4];
+//    GPUImageWhiteBalanceFilter *filter4 = [[GPUImageWhiteBalanceFilter alloc] init];
+//    filter4.temperature = 4900;
+//    filter4.tint = 100;
+//    [filter3 addTarget:filter4];
+    
+     GPUImageGammaFilter *filter4 = [[GPUImageGammaFilter alloc] init];
+     filter4.gamma = 0.7;
+  //  filter4.brightness = 0.5;
+     [filter3 addTarget:filter4];
+    
+    GPUImageUnsharpMaskFilter *filter5 = [[GPUImageUnsharpMaskFilter alloc] init];
+    filter5.intensity = 5;
+  //  filter5.contrast = 0.8;
+    [filter4 addTarget:filter5];
+    
+    GPUImagePosterizeFilter *filter6 = [[GPUImagePosterizeFilter alloc] init];
+    filter6.colorLevels = 200;
+    [filter5 addTarget:filter6];
+    
+
   
     
     GPUImageFilterGroup *groupFilter = [[GPUImageFilterGroup alloc]init];
@@ -65,8 +81,10 @@
     [groupFilter addTarget:filter2];
     [groupFilter addTarget:filter3];
     [groupFilter addTarget:filter4];
+    [groupFilter addTarget:filter5];
+    [groupFilter addTarget:filter6];
     [groupFilter setInitialFilters:[NSArray arrayWithObjects:filter1, nil]];
-    [groupFilter setTerminalFilter:filter4];
+    [groupFilter setTerminalFilter:filter6];
     
     [groupFilter useNextFrameForImageCapture];
     [stillImageSource addTarget:groupFilter];
